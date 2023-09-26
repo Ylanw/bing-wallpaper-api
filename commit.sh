4632eb46f732731815dd40b8f95b89d9bb6e3e6f
YLan <3572066061@qq.com>
2023-09-26 15:36:41 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -5,8 +5,8 @@ if test -z "$(git status -s)"; then
   echo 'no commit'
   exit 0;
 else
-  git config --local user.email "1982989137@qq.com"
-  git config --local user.name "flow2000"
+  git config --local user.email "3572066061@qq.com"
+  git config --local user.name "yanglan2004511"
   git pull origin master
   git add data/*
   git commit -m "update bing json"
